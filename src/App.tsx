aaf0110b16521ef8020eb07d2e3323aedf49b2f8
TkTnX <timur20-09@mail.ru>
2024-04-09 21:35:55 +0500

    prod

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,7 @@ function App() {
       <Header />
       <main>
         <Routes>
-          {/* 
-      //TODO: Сделать валидацию формы
-      // TODO: При отправлении формы закрывать её.
-      // TODO: Если смогу, сделать отдельную страницу для товара
-    */}
+
 
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
